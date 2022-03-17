ff4fbbb6c7d9e2f70567fd24edec6d3f9bd876ea
ketul marfatia <ketulm27@gmail.com>
2022-03-17 16:07:44 +0530

    initial commit

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,12 +1,70 @@
-import React from 'react';
-import { GithubContext } from '../context/context';
-import styled from 'styled-components';
-import { GoRepo, GoGist } from 'react-icons/go';
-import { FiUsers, FiUserPlus } from 'react-icons/fi';
+import React from 'react'
+import { GithubContext } from '../context/context'
+import styled from 'styled-components'
+import { GoRepo, GoGist } from 'react-icons/go'
+import { FiUsers, FiUserPlus } from 'react-icons/fi'
 
 const UserInfo = () => {
-  return <h2>user info component</h2>;
-};
+  const { githubuser } = React.useContext(GithubContext)
+  const { following, public_repos, followers, public_gists } = githubuser
+
+  const items = [
+    {
+      id: 1,
+      icon: <GoRepo className='icon' />,
+      label: 'repos',
+      value: public_repos,
+      color: 'pink',
+    },
+    {
+      id: 2,
+      icon: <FiUsers className='icon' />,
+      label: 'followers',
+      value: followers,
+      color: 'green',
+    },
+    {
+      id: 3,
+      icon: <FiUserPlus className='icon' />,
+      label: 'following',
+      value: following,
+      color: 'purple',
+    },
+    {
+      id: 4,
+      icon: <GoGist className='icon' />,
+      label: 'gists',
+      value: public_gists,
+      color: 'yellow',
+    },
+  ]
+
+  const Item = ({ icon, label, value, color }) => {
+    return (
+      <article className='item'>
+        <span className={color}>{icon}</span>
+        <div>
+          <h3>{value}</h3>
+          <p>{label}</p>
+        </div>
+      </article>
+    )
+  }
+
+  return (
+    <section className='section'>
+      <Wrapper className='section-center'>
+        {items.map((item) => {
+          return (
+            <p>
+              <Item key={item.id} {...item}></Item>
+            </p>
+          )
+        })}
+      </Wrapper>
+    </section>
+  )
+}
 
 const Wrapper = styled.section`
   display: grid;
@@ -58,6 +116,6 @@ const Wrapper = styled.section`
       color: #f0b429;
     }
   }
-`;
+`
 
-export default UserInfo;
+export default UserInfo
--- src/index.js
+++ src/index.js
@@ -1,19 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import { GithubProvider } from './context/context';
-import { Auth0Provider } from '@auth0/auth0-react';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
+import { GithubProvider } from './context/context'
+import { Auth0Provider } from '@auth0/auth0-react'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain='dev-ip57yp3b.us.auth0.com'
+      clientId='qMx0x1hq7FOP0yrDnaLCEzczUQh7W52C'
+      redirectUri={window.location.origin}
+      cacheLocation='localstorage'
+    >
+      <GithubProvider>
+        <App />
+      </GithubProvider>
+    </Auth0Provider>
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
